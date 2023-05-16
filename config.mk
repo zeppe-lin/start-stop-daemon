09ad855d5dbb5e7a3fac616711856ff6349de08f
Alexandr Savca <alexandr.savca89@gmail.com>
2023-05-17 01:11:26 +0300

    config.mk: indent fix

--- config.mk
+++ config.mk
@@ -1,16 +1,16 @@
 # project metadata
-NAME = start-stop-daemon
-VERSION = 1.21
-DIST = ${NAME}-${VERSION}
+NAME      = start-stop-daemon
+VERSION   = 1.21
+DIST      = ${NAME}-${VERSION}
 
 # paths
-PREFIX = /usr/local
+PREFIX    = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
 # flags
-CPPFLAGS = -DVERSION=\"${VERSION}\" -DDISTRO=\"Zeppe-Lin\"
-CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Wformat
-LDFLAGS  =
+CPPFLAGS  = -DVERSION=\"${VERSION}\" -DDISTRO=\"Zeppe-Lin\"
+CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Wformat
+LDFLAGS   =
 
 # compiler and linker
 CC = cc
